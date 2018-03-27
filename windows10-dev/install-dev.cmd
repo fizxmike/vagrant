1dcde7ac31b1561d027bd2853644a108185d20b3
Jan van Veldhuizen <jvv@onguard.com>
2018-03-27 20:20:22 +0200

    updated wercker file

--- windows10-dev/install-dev.cmd
+++ windows10-dev/install-dev.cmd
@@ -1,3 +1,3 @@
 choco install -y git poshgit jdk8 nodejs visualstudiocode winscp sql-server-management-studio
 
-
+choco install visualstudio2017enterprise --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US"
--- windows10-dev/win10dev.bat
+++ windows10-dev/win10dev.bat
@@ -1,10 +1,6 @@
 set starttime=%time%
 vagrant destroy --force
 vagrant up
-@echo off
-echo "Now install Visual Studio manually"
-echo "Press any key when ready..."
-pause
 vagrant package --output win10dev.box
 rem vagrant box add win10dev.box --name win10dev --force --clean
 vagrant destroy --force
