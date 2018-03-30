23d807f75e4fd132225feef908efc14e4f049ac4
Jan van Veldhuizen <jan@van-veldhuizen.nl>
2018-03-30 15:00:51 +0200

    optimizations

--- vagrant/dev-vs2017/customize.cmd
+++ vagrant/dev-vs2017/customize.cmd
@@ -17,24 +17,7 @@ dism /online /Enable-Feature /FeatureName:TelnetClient /NoRestart
 REM Disable Windows Media Player
 dism /online /Disable-Feature /FeatureName:WindowsMediaPlayer /NoRestart
 
-REM install basic development tools
-choco install -y git poshgit jdk8 nodejs visualstudiocode winscp sql-server-management-studio
-
-REM install SqlServer Management Studio
-choco feature enable -n allowGlobalConfirmation
-choco install -y sql-server-management-studio
-
-REM install Visual Studio 2017 Enterprise (do NOT use the --quiet option here, otherwise the setup will fail)
-c:\vs2017setup\vs_setup.exe --includeRecommended --includeOptional --passive --norestart --wait ^
- --add Microsoft.VisualStudio.Workload.CoreEditor ^
- --add Microsoft.VisualStudio.Workload.Azure ^
- --add Microsoft.VisualStudio.Workload.Data ^
- --add Microsoft.VisualStudio.Workload.ManagedDesktop ^
- --add Microsoft.VisualStudio.Workload.NetCoreTools ^
- --add Microsoft.VisualStudio.Workload.NetWeb ^
- --add Microsoft.VisualStudio.Workload.Office ^
- --add Microsoft.VisualStudio.Workload.VisualStudioExtension ^
- --add Component.GitHub.VisualStudio ^
- --add Microsoft.VisualStudio.Component.LinqToSql
+REM just in case this service is still set to delayed
+sc config "WinRM" start=auto
 
 REM --layout c:\xxxx --all --clean
