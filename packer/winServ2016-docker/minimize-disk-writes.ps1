c05a32d87fb54527ea6b5927b708c9ef3371a035
Michael Folkerts <mmfolkerts@gmail.com>
2018-04-21 08:23:57 -0500

    added back vs 2017 ce

--- packer/winServ2016-docker/minimize-disk-writes.ps1
+++ packer/winServ2016-docker/minimize-disk-writes.ps1
@@ -11,27 +11,19 @@ Set-Service -Name "DPS" -StartupType disabled
 Write-Output "Disabling Registry Backups"
 schtasks /change /tn "microsoft\windows\Registry\RegIdleBackup" /disable
 
-# Not supported on windows server
-# Write-Output "Disabling System Restore"
-# Disable-ComputerRestore -Drive $env:SystemDrive 
-# Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" -Name "DisableSR" -Value 1
+# I think this breaks dotnetcore-sdk install with choco?????
+# Write-Output "Disabling Automatic Windows Update"
+# Stop-Service -Name "wuauserv" -Force
+# Set-Service -Name "wuauserv" -StartupType disabled
 
-Write-Output "Disabling Automatic Windows Update"
-Stop-Service -Name "wuauserv" -Force
-Set-Service -Name "wuauserv" -StartupType disabled
+# $WindowsUpdatePath = "HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\"
+# $AutoUpdatePath = "HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
 
-$WindowsUpdatePath = "HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\"
-$AutoUpdatePath = "HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+# If(Test-Path -Path $WindowsUpdatePath) {
+#     Remove-Item -Path $WindowsUpdatePath -Recurse
+# }
 
-If(Test-Path -Path $WindowsUpdatePath) {
-    Remove-Item -Path $WindowsUpdatePath -Recurse
-}
-
-New-Item -Path $WindowsUpdatePath
-New-Item -Path $AutoUpdatePath
-Set-ItemProperty -Path $AutoUpdatePath -Name NoAutoUpdate -Value 1
-
-# not supported on windows server
-# Write-Output "Switching Hibernation off"
-# powercfg /hibernate off
+# New-Item -Path $WindowsUpdatePath
+# New-Item -Path $AutoUpdatePath
+# Set-ItemProperty -Path $AutoUpdatePath -Name NoAutoUpdate -Value 1
 
